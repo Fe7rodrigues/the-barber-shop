e29b9c0b1cf1d271aa8fd06ad4477f8a6fd8f729
fe7rodrigues@gmail.com <fe7rodrigues@gmail.com>
2023-01-26 12:03:39 -0300

    @media(max-width:768px) foi inicializado

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,3 +2,11 @@ window.addEventListener("scroll", function(){
 var header= document.querySelector("header")
 header.classList.toggle("sticky", window.scrollY > 0)
 })
+
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    navMenu.classList.toggle("active");
+})
